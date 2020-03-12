1aa87e343a89a2a23613f28a7b5d2f3f04f67ff7
Buggy-Virus <oliver_hare@brown.edu>
2020-03-11 23:44:47 -0400

    started on dungeon generation functions

--- Assets/MapGeneration/DungeonGeneration.cs
+++ Assets/MapGeneration/DungeonGeneration.cs
@@ -3,6 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+struct ReqRoom {
+    public int roomType;
+    public Vector3 depRoomPoint;
+}
+
+struct Room {
+    public List<int> reqRooms;
+}
+
+struct RoomWeight {
+    public int roomType;
+    public float weight;
+}
+
 public class DungeonGeneration {
-    
+
+    static Dictionary<int, Room> rooms = new Dictionary<int, Room>{
+
+    };
+
+    static int pickFirstRoom(List<int> roomQueue, System.Random random) {
+
+    }
+
+    static int pickRoom(Vector3 origin, Vector3 end, List<int> roomQueue, List<RoomWeight> roomWeights, System.Random random) {
+
+    }
+
+    static int[,,] placeRoom(Vector3 origin, Vector3 end, Room room, System.Random random) {
+
+    }
+
+    static public int[,,] GenerateDungeonSkeleton(int height, int length, int width, System.Random random) {
+        int[,,] skeleton = new int[length, height, width];
+
+        float complexity;
+
+        Dictionary<int, int> roomMaxes;
+        Dictionary<int, int> roomCounts;
+
+        List<ReqRoom> requiredRooms;
+
+        List<RoomWeight> roomWeights; 
+
+        int firstRoom = pickFirstRoom(roomQueue, random);
+
+
+    }
 }
